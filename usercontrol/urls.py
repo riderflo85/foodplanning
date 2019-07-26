cc54b8ee49f2a7d4d0e0f6fdcefc0406c927cadd
Florent <grenaille.florent85@gmail.com>
2019-07-26 15:56:25 +0200

    Add sign_up page

--- usercontrol/urls.py
+++ usercontrol/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'usercontrol'
 urlpatterns = [
     path('', views.sign_in, name="sign_in"),
+    path('sign_up/', views.sign_up, name="sign_up"),
 ]
--- usercontrol/views.py
+++ usercontrol/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def sign_in(request):
     return render(request, 'usercontrol/sign_in.html')
+
+def sign_up(request):
+    return render(request, 'usercontrol/sign_up.html')
