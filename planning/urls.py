cdc891a292b66a84bb8c0ee193372aa04543e32b
Florent <grenaille.florent85@gmail.com>
2019-07-27 14:05:51 +0200

    Add view and template for planning app django

--- planning/urls.py
+++ planning/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-# app_name = 'planning'
-# urlpatterns = [
-#     path(),
-# ]
+app_name = 'planning'
+urlpatterns = [
+    path('', views.planning, name="planning"),
+]
--- planning/views.py
+++ planning/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def planning(request):
+    return render(request, 'planning/index.html')
