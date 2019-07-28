bb0a97724f1b9bd0d52651caae5477e88eb6e5af
Florent <grenaille.florent85@gmail.com>
2019-07-28 16:40:37 +0200

    Add fields for Planning model

--- planning/models.py
+++ planning/models.py
@@ -12,6 +12,20 @@ class Planning(models.Model):
     sunday = models.CharField(max_length=160)
     moment_day = models.CharField(max_length=2, null=False)
     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    notif_mo = models.BooleanField(default=False)
+    notif_tu = models.BooleanField(default=False)
+    notif_we = models.BooleanField(default=False)
+    notif_th = models.BooleanField(default=False)
+    notif_fr = models.BooleanField(default=False)
+    notif_sa = models.BooleanField(default=False)
+    notif_su = models.BooleanField(default=False)
+    text_notif_mo = models.TextField(null=True)
+    text_notif_tu = models.TextField(null=True)
+    text_notif_we = models.TextField(null=True)
+    text_notif_th = models.TextField(null=True)
+    text_notif_fr = models.TextField(null=True)
+    text_notif_sa = models.TextField(null=True)
+    text_notif_su = models.TextField(null=True)
 
     class Meta:
         verbose_name = "Planning de la semaine"
