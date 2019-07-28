6b7d9634f86d92a2c08fb62509770ed86a63ebd6
Florent <grenaille.florent85@gmail.com>
2019-07-28 13:07:18 +0200

    Add models for fooddish app and migrate applying

--- fooddish/models.py
+++ fooddish/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Fooddish(models.Model):
+    name = models.CharField(max_length=160, null=False, unique=True)
+
+    class Meta:
+        verbose_name = 'Plat'
+
+    def __str__(self):
+        return self.name
