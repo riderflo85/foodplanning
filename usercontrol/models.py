0e1b9efd6e622f4a9fd9c7bf6bd98ae96e001f1c
Florent <grenaille.florent85@gmail.com>
2019-07-28 13:21:05 +0200

    Add and applying migrations for usercontrol django app

--- usercontrol/models.py
+++ usercontrol/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class PhoneNumber(models.Model):
+    number = models.IntegerField(null=False)
+    id_user = models.OneToOneField(User, on_delete=models.CASCADE)
