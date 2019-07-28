2c98a8ec7d2dd61358f66f627d3a7c3e785110a1
Florent <grenaille.florent85@gmail.com>
2019-07-28 15:16:27 +0200

    Validation of the login form in the sign-in view

--- usercontrol/views.py
+++ usercontrol/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import login, logout, authenticate
 from .forms import SignupForm, LoginForm
 
 def sign_in(request):
-    context = {}
-    form = LoginForm()
+    context = {'error': False,}
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['user']
+            pwd = form.cleaned_data['password']
+            login_user = authenticate(request, username=username, password=pwd)
+            context['user'] = login_user
+
+            if login_user is not None:
+                login(request, user=login_user)
+                return redirect(reverse('planning:index'))
+            else:
+                context['error'] = True
+    else:
+        form = LoginForm()
+
     context['form'] = form
 
     return render(request, 'usercontrol/sign_in.html', context)
