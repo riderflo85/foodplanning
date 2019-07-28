1072c5dbf770ff4ba5e09b91729a1e5602a0c4b0
Florent <grenaille.florent85@gmail.com>
2019-07-28 13:48:53 +0200

    Add file 'forms.py' for use django forms

---
+++ usercontrol/forms.py
@@ -0,0 +1,63 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    user = forms.CharField(
+        label='user',
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Pseudo ou email', 'class': 'form-control'}
+            ),
+    )
+    password = forms.CharField(
+        label='password',
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Mot de passe', 'class': 'form-control'}
+            ),
+    )
+
+
+class SignupForm(forms.Form):
+    pseudo = forms.CharField(
+        label='pseudo',
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Pseudo', 'class': 'form-control'}
+        ),
+    )
+    last_name = forms.CharField(
+        label='nom',
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Nom', 'class':'form-control'}
+        ),
+    )
+    first_name = forms.CharField(
+        label='prénom',
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Prénom', 'class': 'form-control'}
+        ),
+    )
+    email = forms.EmailField(
+        label='email',
+        widget=forms.EmailInput(
+            attrs={'placeholder': 'Email', 'class': 'form-control'}
+        ),
+    )
+    password = forms.CharField(
+        label='password',
+        min_length=8,
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Mot de passe', 'class': 'form-control'}
+        ),
+    )
+    phone_number = forms.CharField(
+        label='phone_number',
+        max_length=9,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Numéro de portable pour être notifié',
+            'class': 'form-control',
+            'pattern': "[0-9]{1}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}"}
+        )
+    )
--- usercontrol/views.py
+++ usercontrol/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .forms import SignupForm
 
 def sign_in(request):
     return render(request, 'usercontrol/sign_in.html')
 
 def sign_up(request):
-    return render(request, 'usercontrol/sign_up.html')
+    context = {}
+    form = SignupForm()
+    context['form'] = form
+
+    return render(request, 'usercontrol/sign_up.html', context)
