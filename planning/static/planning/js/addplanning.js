584fa73d687b5eae4e9ce357756972331d236689
Florent <grenaille.florent85@gmail.com>
2019-08-08 18:54:22 +0200

    Adding animated JS

--- planning/static/planning/js/addplanning.js
+++ planning/static/planning/js/addplanning.js
@@ -13,7 +13,7 @@ function addPlanning() {
         let planningFood = new Planning();
         let username = planningFood.set_user_planning();
 
-        var baliseTitlePlannig = `<p class='lead text-white mt-5' id='userPlanning${planningFood.id_user}'>Planning pour ${username}</p>`;
+        var baliseTitlePlannig = `<p class='lead text-white mt-5 animated rollIn' id='userPlanning${planningFood.id_user}'>Planning pour ${username}</p>`;
         $(baliseTitlePlannig).appendTo(divContent);
         
         planningFood.new_planning(divContent);
--- planning/static/planning/js/removeplanning.js
+++ planning/static/planning/js/removeplanning.js
@@ -3,7 +3,16 @@ function removePlanning(user) {
     var btnValideRemoved = document.getElementById('confirmUserRemovePlanning');
 
     btnValideRemoved.addEventListener('click', function () {
-        $('#userPlanning'+user).remove();
-        $('#table'+user).remove();
+        let user_planning = document.getElementById('userPlanning'+user);
+        let table = document.getElementById('table'+user);
+
+        user_planning.classList.remove('rollIn');
+        table.classList.remove('rollIn');
+        user_planning.classList.add('rollOut');
+        table.classList.add('rollOut');
+        table.addEventListener('animationend', function () {
+            $('#userPlanning'+user).remove();
+            $('#table'+user).remove();
+        });
     });
 }
