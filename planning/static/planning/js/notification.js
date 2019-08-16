7b73b89e09c8bed08606932474d400ed9edfe4e2
Florent <grenaille.florent85@gmail.com>
2019-08-16 17:25:46 +0200

    Adding new function in notification.js for send all the data in the backend script

--- planning/static/planning/js/notification.js
+++ planning/static/planning/js/notification.js
@@ -1,3 +1,36 @@
+function send_notification(date, time, msg) {
+    var csrftoken = getCookie('csrftoken');
+
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: '/notif/set',
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                'date': date,
+                'time': time,
+                'message': msg
+            },
+            success: function (data) {
+                resolve(data);
+            },
+            error: function (error) {
+                reject(error);
+                console.log(error);
+            },
+        });
+    });
+}
+
+
 function addNotification(user) {
     $('#modalBoxNotification').modal();
     var btnNotif = document.getElementById('confirmNotification');
@@ -8,9 +41,25 @@ function addNotification(user) {
         var hours = document.getElementById('timeNotif').value;
         var message = document.getElementById('messageNotif').value;
 
+        const OnSucces = data => {
+            if (data === true) {
+                console.log('La notification a bien été ajouter au gestionnaire de tache');
+            }else{
+                console.warn("Une erreur est survenue, la notif n'a pas pu être ajoutée");
+            }
+        }
+
+        const onFailure = error => {
+            console.warn(error);
+        }
+
+        const newNotif = send_notification(date, hours, message);
+        newNotif.then(OnSucces).catch(onFailure);
+
         console.log(days);
         console.log(date);
         console.log(hours);
         console.log(message);
+
     });
 }
