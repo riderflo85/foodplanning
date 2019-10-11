58b3f2ab11164179e4bcbecaa6e457659229efbb
Florent <grenaille.florent85@gmail.com>
2019-10-11 18:53:15 +0200

    Add new tests

--- planning/tests/tests_planning_page.py
+++ planning/tests/tests_planning_page.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
-from planning.views import planning
-from planning.models import PlanningAm
+from planning.views import planning, planning_pm
+from planning.models import PlanningAm, PlanningPm
 from fooddish.models import Fooddish
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
@@ -10,6 +10,13 @@ from django.contrib.auth import login, authenticate
 class PlanningPageTestCase(TestCase):
     def setUp(self):
         self.cli = Client()
+        self.user = User.objects.create_user(
+            'userTest',
+            'testuser@test.com',
+            'PwdUserTest'
+        )
+        self.days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+        self.allUser = User.objects.all()
 
     def test_status_code_page_planning(self):
         rep = self.cli.get('/planning/')
@@ -22,6 +29,36 @@ class PlanningPageTestCase(TestCase):
     def test_view_page_planning(self):
         rep = self.cli.get('/planning/')
         self.assertEqual(rep.resolver_match.func, planning)
+
+    def test_context_page_planning(self):
+        self.cli.login(username=self.user.username, password='PwdUserTest')
+        rep = self.cli.get('/planning/')
+        self.assertEqual(rep.context['days'], self.days)
+        self.assertEqual(len(rep.context['users']), len(self.allUser))
+        self.assertEqual(rep.context['users'][0].id, self.allUser[0].id)
+        self.assertEqual(rep.context['dishs'], {})
+        self.assertEqual(len(rep.context['planning']), len(PlanningAm.objects.all()))
+
+    def test_context_page_planning_pm(self):
+        self.cli.login(username=self.user.username, password='PwdUserTest')
+        rep = self.cli.get('/planning/pm')
+        self.assertEqual(rep.context['days'], self.days)
+        self.assertEqual(len(rep.context['users']), len(self.allUser))
+        self.assertEqual(rep.context['users'][0].id, self.allUser[0].id)
+        self.assertEqual(rep.context['dishs'], {})
+        self.assertEqual(len(rep.context['planning']), len(PlanningPm.objects.all()))
+
+    def test_status_code_page_planning_pm(self):
+        rep = self.cli.get('/planning/pm')
+        self.assertEqual(rep.status_code, 200)
+
+    def test_template_page_planning_pm(self):
+        rep = self.cli.get('/planning/pm')
+        self.assertTemplateUsed('planning/pm.html')
+
+    def test_view_page_planning_pm(self):
+        rep = self.cli.get('/planning/pm')
+        self.assertEqual(rep.resolver_match.func, planning_pm)
 
 class IntegrationTestCase(TestCase):
     def setUp(self):
@@ -47,6 +84,17 @@ class IntegrationTestCase(TestCase):
         planning.moment_day = "am"
         planning.id_user = self.user
         planning.save()
+        planning2 = PlanningPm()
+        planning2.monday = "Plat 1"
+        planning2.tuesday = "Plat 2"
+        planning2.wednesday = "Plat 3"
+        planning2.thursday = "Plat 4"
+        planning2.friday = "Plat 5"
+        planning2.saturday = "Plat 6"
+        planning2.sunday = "Plat 7"
+        planning2.moment_day = "am"
+        planning2.id_user = self.user
+        planning2.save()
         foo = Fooddish()
         foo.name = "Tacos"
         foo.save()
@@ -54,11 +102,12 @@ class IntegrationTestCase(TestCase):
         foo2.name = "Burger"
         foo2.save()
         self.plann = planning
+        self.plann2 = planning2
         self.food = foo
         self.food2 = foo2
 
     def test_create_new_planning(self):
-        data = {
+        data1 = {
             'days1': 'Plat 1',
             'days2': 'Plat 2',
             'days3': 'Plat 3',
@@ -68,11 +117,23 @@ class IntegrationTestCase(TestCase):
             'days7': 'Plat 7',
             'momentDay' : 'am'
             }
+        data2 = {
+            'days1': 'Plat 1',
+            'days2': 'Plat 2',
+            'days3': 'Plat 3',
+            'days4': 'Plat 4',
+            'days5': 'Plat 5',
+            'days6': 'Plat 6',
+            'days7': 'Plat 7',
+            'momentDay' : 'pm'
+            }
         self.cli.login(username=self.user2.username, password='PwdUserTest')
-        rep = self.cli.post('/planning/set', data)
-        self.assertTrue(rep.json()['ServeurResponse'])
+        rep1 = self.cli.post('/planning/set', data1)
+        self.assertTrue(rep1.json()['ServeurResponse'])
+        rep2 = self.cli.post('/planning/set', data2)
+        self.assertTrue(rep2.json()['ServeurResponse'])
 
-    def test_remove_planning(self):
+    def test_remove_planning_am(self):
         data = {'id_planning': self.plann.pk, 'momentDay': 'am'}
         rep = self.cli.post('/planning/remove', data)
         self.assertTrue(rep.json()['ServeurResponse'])
@@ -81,7 +142,7 @@ class IntegrationTestCase(TestCase):
         rep = self.cli.get('/planning/remove')
         self.assertFalse(rep.json()['ServeurResponse'])
 
-    def test_update_planning(self):
+    def test_update_planning_am(self):
         data = {
             'id': self.plann.id,
             'monday': self.food.id,
@@ -98,6 +159,30 @@ class IntegrationTestCase(TestCase):
         self.assertTrue(rep.json()['ServeurResponse'])
 
         check = PlanningAm.objects.get(id=self.plann.pk).monday
+        self.assertEqual(check, self.food.name)
+
+    def test_remove_planning_pm(self):
+        data = {'id_planning': self.plann2.pk, 'momentDay': 'pm'}
+        rep = self.cli.post('/planning/remove', data)
+        self.assertTrue(rep.json()['ServeurResponse'])
+
+    def test_update_planning_pm(self):
+        data = {
+            'id': self.plann2.id,
+            'monday': self.food.id,
+            'tuesday': self.food.id,
+            'wednesday': self.food.id,
+            'thursday': self.food2.id,
+            'friday': self.food2.id,
+            'saturday': self.food.id,
+            'sunday': self.food2.id,
+            'momentDay': 'pm'
+        }
+
+        rep = self.cli.post('/planning/update', data)
+        self.assertTrue(rep.json()['ServeurResponse'])
+
+        check = PlanningPm.objects.get(id=self.plann2.pk).monday
         self.assertEqual(check, self.food.name)
 
     def test_duplicate_planning_database_error_for_user(self):
