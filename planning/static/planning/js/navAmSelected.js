d55649976f45d9d892af39ca1e4eb0c04764abfb
Florent <grenaille.florent85@gmail.com>
2019-10-19 20:40:12 +0200

    Nav bar link modified style CSS

---
+++ planning/static/planning/js/navAmSelected.js
@@ -0,0 +1,15 @@
+var itemsNavMenu = [
+    document.getElementById('nav-am'),
+    document.getElementById('nav-pm'),
+    document.getElementById('nav-dishs')
+];
+
+for (const i of itemsNavMenu) {
+    for (const e of i.classList) {
+        if (e === 'this-page') {
+            i.classList.remove('this-page');
+        }
+    }
+}
+
+itemsNavMenu[0].classList.add('this-page');
