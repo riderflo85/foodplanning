1a65e5b80857648c4db8a05d516c5806724c3689
Florent <grenaille.florent85@gmail.com>
2019-11-09 16:44:23 +0100

    Add docstring

--- fooddish/forms.py
+++ fooddish/forms.py
@@ -4,6 +4,8 @@ from .list_all_dish import list_all_dish
 
 
 class AddDishForms(forms.Form):
+    """ Form used for user add new dish in the database """
+
     name_dish = forms.CharField(
         label='name',
         max_length=160,
@@ -13,6 +15,8 @@ class AddDishForms(forms.Form):
     )
 
 class DelDishForms(forms.Form):
+    """ Form used for user to delete a dish in the database """
+
     del_dish = forms.ChoiceField(
         label='delete',
         choices=lazy(list_all_dish, tuple),
