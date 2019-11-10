8b829bcacc1f4594ceaeeec0614eb56b76e4254e
Florent <grenaille.florent85@gmail.com>
2019-11-10 11:40:33 +0100

    Added a check to see if the user has enabled SMS notification

--- planning/static/planning/js/notification.js
+++ planning/static/planning/js/notification.js
@@ -32,37 +32,41 @@ function send_notification(date, time, msg) {
 
 
 function addNotification(user) {
-    $('#modalBoxNotification').modal();
-    var btnNotif = document.getElementById('confirmNotification');
-
-    btnNotif.addEventListener('click', function () {
-        var days = document.getElementById('selectDay').value;
-        var date = document.getElementById('dateNotif').value;
-        var hours = document.getElementById('timeNotif').value;
-        var message = document.getElementById('messageNotif').value;
-
-        const OnSucces = data => {
-            if (data['Response'] === true) {
-                console.log('La notification a bien été ajouter au gestionnaire de tache');
-                var dayForBadge = document.getElementById(`dish-day${days}-user${user}`);
-                var baliseBadge = ` <i class='badge badge-pill badge-warning'>notif</i>`;
-                $(baliseBadge).appendTo(dayForBadge);
-            }else{
-                console.warn("Une erreur est survenue, la notif n'a pas pu être ajoutée");
+    if ($('#checkUseSms')[0].textContent == 'False'){
+        $('#modalBoxRemoveNewPlanning').modal();
+    }else{
+        $('#modalBoxNotification').modal();
+        var btnNotif = document.getElementById('confirmNotification');
+    
+        btnNotif.addEventListener('click', function () {
+            var days = document.getElementById('selectDay').value;
+            var date = document.getElementById('dateNotif').value;
+            var hours = document.getElementById('timeNotif').value;
+            var message = document.getElementById('messageNotif').value;
+    
+            const OnSucces = data => {
+                if (data['Response'] === true) {
+                    console.log('La notification a bien été ajouter au gestionnaire de tache');
+                    var dayForBadge = document.getElementById(`dish-day${days}-user${user}`);
+                    var baliseBadge = ` <i class='badge badge-pill badge-warning'>notif</i>`;
+                    $(baliseBadge).appendTo(dayForBadge);
+                }else{
+                    console.warn("Une erreur est survenue, la notif n'a pas pu être ajoutée");
+                }
             }
-        }
-
-        const onFailure = error => {
-            console.warn(error);
-        }
-
-        const newNotif = send_notification(date, hours, message);
-        newNotif.then(OnSucces).catch(onFailure);
-
-        console.log(days);
-        console.log(date);
-        console.log(hours);
-        console.log(message);
-
-    });
+    
+            const onFailure = error => {
+                console.warn(error);
+            }
+    
+            const newNotif = send_notification(date, hours, message);
+            newNotif.then(OnSucces).catch(onFailure);
+    
+            console.log(days);
+            console.log(date);
+            console.log(hours);
+            console.log(message);
+    
+        });
+    }
 }
