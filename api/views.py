9be9e8986d0a52a920c0f1fa4bf9e5dd8f7afd67
Florent <grenaille.florent85@gmail.com>
2019-12-15 13:09:47 +0100

    Create 'get_dish' view for validate all tests

--- api/views.py
+++ api/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
 from usercontrol.models import User
 from planning.models import PlanningAm, PlanningPm
 
-# Create your views here.
+
+def get_dish(request):
+    """ Return the dish requested by the request on the API """
+
+    if request.method == 'POST':
+        try:
+            user = User.objects.get(username=request.POST['username'])
+            day = request.POST['day']
+            am_or_pm = request.POST['amOrPm']
+
+            if am_or_pm == "am":
+                plann = PlanningAm.objects.get(id_user=user.pk)
+                dish = getattr(plann, day)
+                return JsonResponse({"dish": dish})
+
+            elif am_or_pm == "pm":
+                plann = PlanningPm.objects.get(id_user=user.pk)
+                dish = getattr(plann, day)
+                return JsonResponse({"dish": dish})
+
+        except ObjectDoesNotExist:
+            return JsonResponse({"error": True})
